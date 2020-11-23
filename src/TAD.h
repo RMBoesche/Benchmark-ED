e0c6a75a4c5825c62b7122ed0e5bc7e157ab1b65
RMBoesche <rodrigo.boesche@gmail.com>
2020-11-23 17:36:12 -0300

    Retirei algumas partes que ficaram redundantes, é possivel em valores ordenados fazer a avaliacao até 20.000 dados e em dados aleatorios 100.000, vou tentar transformar os cmps em long int.

--- src/TAD.h
+++ src/TAD.h
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#define TAMANHO 1000000
+
+#define TAMANHO 100000
 
 /*===============================================================================
                         Início - Lista Simplesmente Encadeada
--- src/main.c
+++ src/main.c
@@ -5,26 +5,34 @@ int main()
 	NODO_LSE *listaLSE = NULL, *nodoLSE = NULL;
 	NODO_ABP *arvABP = NULL, *nodoABP = NULL;
 	NODO_AVL *arvAVL = NULL, *nodoAVL = NULL;
-	int num, opcao, i; //opcao eh uma variavel pra determinar se os valores serao aleatorios ou ordenados
-	long int totalvalores = 0;
-	int vet[TAMANHO]; //ordenados = 0 e aleatorios = 1
-	int cmpsLSE, cmpsABP, cmpsAVL;
-	float tLSE = 0, tABP = 0, tAVL = 0;
+
+	int i, opcao; //opcao eh uma variavel pra determinar se os valores serao aleatorios ou ordenados
 	int ok = 0, ciclo = 0;
+
+	int cmpsLSE = 0, cmpsABP = 0, cmpsAVL = 0;
+	float tLSE = 0, tABP = 0, tAVL = 0;
+
+	int dados[TAMANHO]; //ordenados = 0 e aleatorios = 1
+	int dadosTotal = 0;
 
 	clock_t start, end;
 
-	printf("Escolha o numero de valores a serem carregados: \n");
-	scanf("%ld", &totalvalores);
+	printf("Escolha o numero de dados a serem carregados: \n");
+	scanf("%d", &dadosTotal);
 
 	printf("Escolha se os valores devem ser ordenados (0) ou aleatorios (1): \n");
 	scanf("%d", &opcao);
-	srand(time(NULL));
 
-	if (opcao == 1)
+	if (opcao == 0)
+		for (i = 0; i < dadosTotal; i++)
+			dados[i] = i;
+
+	else if (opcao == 1)
 	{
-		for (i = 0; vet[i] < TAMANHO; i++) //CRIA O VETOR COM OS VALORES ALEATORIOS
-			vet[i] = rand() % 1000000;
+		srand(time(NULL));
+
+		for (i = 0; i < dadosTotal; i++) //CRIA O VETOR COM OS VALORES ALEATORIOS
+			dados[i] = rand() % dadosTotal;
 	}
 
 	for (ciclo = 0; ciclo < 3; ciclo++)
@@ -35,76 +43,48 @@ int main()
 
 		start = clock();
 
-		for (num = 0; num < totalvalores; num++)
-		{
-			if (opcao == 1)
-				listaLSE = inserirLSE(listaLSE, vet[num]);
-			else
-				listaLSE = inserirLSE(listaLSE, num);
-		}
+		for (i = 0; i < dadosTotal; i++)
+			listaLSE = inserirLSE(listaLSE, dados[i]);
 
-		for (num = 0; num <= totalvalores; num += 10)
-		{
-			if (opcao == 1)
-				nodoLSE = consultarLSE(listaLSE, vet[num], &cmpsLSE);
-			else
-				nodoLSE = consultarLSE(listaLSE, num, &cmpsLSE);
-		}
+		for (i = 0; i < dadosTotal; i += dadosTotal / 10)
+			nodoLSE = consultarLSE(listaLSE, dados[i], &cmpsLSE);
 
 		end = clock();
 
 		tLSE += ((float)(end - start) / CLOCKS_PER_SEC * 1000);
 
+		listaLSE = destruirLSE(listaLSE);
+
 		//============================================================================
 
 		start = clock();
 
-		for (num = 0; num < totalvalores; num++)
-		{
-			if (opcao == 1)
-				arvABP = inserirABP(arvABP, vet[num], &cmpsABP);
-			else
-				arvABP = inserirABP(arvABP, num, &cmpsABP);
-		}
+		for (i = 0; i < dadosTotal; i++)
+			arvABP = inserirABP(arvABP, dados[i], &cmpsABP);
 
-		for (num = 0; num <= totalvalores; num += 10)
-		{
-			if (opcao == 1)
-				nodoABP = consultarABP(arvABP, vet[num], &cmpsABP);
-			else
-				nodoABP = consultarABP(arvABP, num, &cmpsABP);
-		}
+		for (i = 0; i < dadosTotal; i += dadosTotal / 10)
+			nodoABP = consultarABP(arvABP, dados[i], &cmpsABP);
 
 		end = clock();
 
 		tABP += ((float)(end - start) / CLOCKS_PER_SEC * 1000);
 
+		arvABP = destruirABP(arvABP);
+
 		//============================================================================
 
 		start = clock();
 
-		for (num = 0; num < totalvalores; num++)
-		{
-			if (opcao == 1)
-				arvAVL = inserirAVL(arvAVL, vet[num], &ok, &cmpsAVL);
-			else
-				arvAVL = inserirAVL(arvAVL, num, &ok, &cmpsAVL);
-		}
+		for (i = 0; i < dadosTotal; i++)
+			arvAVL = inserirAVL(arvAVL, dados[i], &ok, &cmpsAVL);
 
-		for (num = 0; num <= totalvalores; num += 10)
-		{
-			if (opcao == 1)
-				nodoAVL = consultarAVL(arvAVL, vet[num], &cmpsAVL);
-			else
-				nodoAVL = consultarAVL(arvAVL, num, &cmpsAVL);
-		}
+		for (i = 0; i < dadosTotal; i += dadosTotal / 10)
+			nodoAVL = consultarAVL(arvAVL, dados[i], &cmpsAVL);
 
 		end = clock();
 
 		tAVL += ((float)(end - start) / CLOCKS_PER_SEC * 1000);
 
-		listaLSE = destruirLSE(listaLSE);
-		arvABP = destruirABP(arvABP);
 		arvAVL = destruirAVL(arvAVL);
 	}
 
